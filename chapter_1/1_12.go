f91cd5a7ffb3117d55f392a889773bceb07674c3
zhaopengcheng <zhaopengcheng@babeltime.com>
2017-03-01 22:00:18 +0800

    1_12

--- chapter_1/1_12.go
+++ chapter_1/1_12.go
@@ -13,25 +13,27 @@ var count int
 
 func main() {
 	http.HandleFunc("/", handler) // each request calls handler
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+
 	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 // handler echoes the Path component of the request URL r.
 func handler(w http.ResponseWriter, r *http.Request) {
-	// fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
-	mu.Lock()
-
-	count++
-
-	mu.Unlock()
+	// fmt.Fprintf(w, "URL.Path = %q\n", r)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
 }
 
 func counter(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprintf(w, "URL.Path====%q\n", r.URL.Path)
 	mu.Lock()
 
-	fmt.Fprintf("counter is %d", count)
+	fmt.Fprintf(w, "Count %d\n", count)
 
 	mu.Unlock()
 }
