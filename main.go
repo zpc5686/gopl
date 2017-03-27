61c871caf0783dbd36c241bf220959fa1ea10501
System Administrator <root@192.168.0.102>
2017-03-27 23:44:31 +0800

    gob编码

--- main.go
+++ main.go
@@ -1,41 +1,44 @@
 package main
 
 import (
-	"bufio"
+	// "bufio"
 	"bytes"
 	"encoding/gob"
 	"fmt"
 	// "log"
-	"io"
-	// "io/ioutil"
+	// "io"
+	"io/ioutil"
 	"os"
 )
 
-type TTactic struct {
-	TacticInfo map[int32][]uint32
-}
+// type TTactic struct {
+	// TacticInfo map[int32][]uint32
+// }
 
 func main() {
-	gob.Register(make(map[string]interface{}))
+	gob.Register(make(map[string][]uint32))
 	gob.Register(make([]interface{}, 0))
 	f, err := os.Open("./tactic")
 	if err != nil {
 		panic(err)
 	}
-	var ret TTactic
-	reader := bufio.NewReader(f)
-	bts, err := reader.ReadBytes(io.SeekEnd)
-	if err != nil {
-		fmt.Println("readbytes", err)
-		panic(err)
-	}
-	fmt.Println("...", bts)
-	buffer := bytes.NewBuffer(bts)
-	dec := gob.NewDecoder(buffer)
-	err = dec.Decode(&ret)
+	var ret map[int32][]uint32	
+	// reader := bufio.NewReader(f)
+	// fmt.Println("io SeekEnd",io.SeekEnd)
+	bts, err := ioutil.ReadAll(f)
 	if err != nil {
 		fmt.Println("...", err)
 		panic(err)
 	}
-	fmt.Println("t%#v", ret.TacticInfo)
+	// fmt.Println("...", len(bts))
+	// temp :=bts[0:len(bts)-2]
+	// fmt.Println("...",len(temp))
+	buffer := bytes.NewBuffer(bts)
+	dec := gob.NewDecoder(buffer)
+	err = dec.Decode(&ret)
+	if err != nil {
+		// fmt.Println("...", err)
+		panic(err)
+	}
+	fmt.Println("t%#v", ret)
 }
